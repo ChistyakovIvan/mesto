2c1a33e72e0f2760a0d1e64edd950de0f673e095
Chistyakov Ivan <chistyakov.ivan@gmail.com>
2020-12-25 20:10:49 +0300

    feat/ validate.js added

---
+++ script/validate.js
@@ -0,0 +1,7 @@
+const validationConfig = {
+	formSelector: ".popup__form",
+	inputSelector: ".popup__form-item",
+	submitButtonSelector: ".popup__submit-button",
+	inputInvalidClass: "popup__input_state_invalid",
+	buttonInvalidClass: "popup__submit-button_invalid",
+};
