3c06d851b08785a3ac1c2c23ca2315e1be316679
Chistyakov Ivan <chistyakov.ivan@gmail.com>
2020-12-25 20:27:34 +0300

    feat/ span to html added

--- script/validate.js
+++ script/validate.js
@@ -2,6 +2,6 @@ const validationConfig = {
 	formSelector: ".popup__form",
 	inputSelector: ".popup__form-item",
 	submitButtonSelector: ".popup__submit-button",
-	inputInvalidClass: "popup__input_state_invalid",
+	inputInvalidClass: "popup__form-item_state_invalid",
 	buttonInvalidClass: "popup__submit-button_invalid",
 };
